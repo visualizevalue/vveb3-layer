29e8d3f508f51ccc48d5745071837f2fa0b46b0b
jalil <jalil@1001.digital>
2025-05-07 02:13:11 +0200

    Install rabby icon

--- components/Connect.client.vue
+++ components/Connect.client.vue
@@ -33,6 +33,7 @@ const ICONS = {
   'Coinbase Wallet': 'coinbase.svg',
   MetaMask: 'metamask.svg',
   Phantom: 'phantom.svg',
+  Rabby: 'rabby.svg',
   Rainbow: 'rainbow.svg',
   WalletConnect: 'walletconnect.svg',
 }
